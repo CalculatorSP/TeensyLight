70e1a56d39c712c12471356b1431b5bc8fa0b6ad
CalculatorSP <mike.handidate@gmail.com>
2016-12-25 13:16:02 -0500

    Add letterbox compensation

--- VS/TeensyLight/TeensyLight/Main.cpp
+++ VS/TeensyLight/TeensyLight/Main.cpp
@@ -13,6 +13,9 @@ using namespace cv;
 
 static const LPCWSTR comport = L"COM7";
 
+static void _updatePixels(const Mat& frame, TeensyLightController& controller);
+static int _getTopRow(const Mat& frame);
+
 int main(int argc, const char** argv)
 {
     TeensyLightController controller;
@@ -28,26 +31,43 @@ int main(int argc, const char** argv)
             break;
 
         resize(frame, frame, Size(HOR_PIXELS, VER_PIXELS), INTER_LINEAR);
-
-        uint8_t index = 0;
-        for (int i = VER_PIXELS - 1; i > 0; --i)
-        {
-            Vec4b color = frame.at<Vec4b>(i, 0);
-            controller.setPixel(index++, color[2], color[1], color[0], 0);
-        }
-        for (int i = 1; i < HOR_PIXELS - 1; ++i)
-        {
-            Vec4b color = frame.at<Vec4b>(0, i);
-            controller.setPixel(index++, color[2], color[1], color[0], 0);
-        }
-        for (int i = 1; i < VER_PIXELS; ++i)
-        {
-            Vec4b color = frame.at<Vec4b>(i, HOR_PIXELS - 1);
-            controller.setPixel(index++, color[2], color[1], color[0], 0);
-        }
-
-        controller.show();
+        _updatePixels(frame, controller);
     }
 
     return 0;
+}
+
+static void _updatePixels(const Mat& frame, TeensyLightController& controller)
+{
+    int topRow = _getTopRow(frame);
+
+    uint8_t index = 0;
+    for (int i = frame.rows - 1; i > 0; --i)
+    {
+        Vec4b color = frame.at<Vec4b>(i, 0);
+        controller.setPixel(index++, color[2], color[1], color[0], 0);
+    }
+    for (int i = 1; i < frame.cols - 1; ++i)
+    {
+        Vec4b color = frame.at<Vec4b>(topRow, i);
+        controller.setPixel(index++, color[2], color[1], color[0], 0);
+    }
+    for (int i = 1; i < frame.rows; ++i)
+    {
+        Vec4b color = frame.at<Vec4b>(i, frame.cols - 1);
+        controller.setPixel(index++, color[2], color[1], color[0], 0);
+    }
+
+    controller.show();
+}
+
+static int _getTopRow(const Mat& frame)
+{
+    int blackCount = 0;
+    for (int i = 0; i < frame.cols; ++i)
+        for (int c = 0; c < frame.channels(); ++c)
+            if (frame.at<uint8_t>(0, i, c) == 0x00)
+                ++blackCount;
+
+    return (blackCount > 5 * frame.channels()) ? 1 : 0;
 }
